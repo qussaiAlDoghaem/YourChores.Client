d21cff9ff24a39f1539022b347a3c7b98da40dee
qussai97 <qussai.doghaem@gmail.com>
2023-08-21 22:58:10 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React ,{useState} from 'react';
+import { StatusBar  } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [weather,setWeather] = useState("Lodding .....");
+
+  
+    const getWeather = async ()=>{
+      
+      var res = await fetch('http://192.168.20.10:5001/WeatherForecast') ;
+      var data = await res.json();
+  
+      setWeather(data[0].summary);
+  }
+  getWeather();
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{weather}</Text>
     </View>
   );
 }
